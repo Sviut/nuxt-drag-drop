c77aad0051bd29045471f9723a1bd351e1b80a0f
Zheleznov Aleksandr Romanovich <azheleznov@ozon.ru>
2023-08-26 18:49:39 +0700

    base cards

---
+++ components/TrelloBoard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {Column} from "~/types";
+import {nanoid} from "nanoid";
+
+const columns = ref<Column[]>([
+  {
+    id: nanoid(),
+    title: 'Backlog',
+    tasks: [
+      {
+        title: "Create marketing landing page", createdAt: new Date(), id: nanoid(),
+      },
+      {
+        title: "Develop cool new feature", createdAt: new Date(), id: nanoid(),
+      },
+      {title: "Fix page nav bug", createdAt: new Date(), id: nanoid()},
+    ],
+  },
+  {title: "Selected for Dev", id: nanoid(), tasks: []},
+  {title: "In Progress", id: nanoid(), tasks: []},
+  {title: "QA", id: nanoid(), tasks: []},
+  {title: "Complete", id: nanoid(), tasks: []},
+])
+</script>
+
+<template>
+  <div class="flex gap-4 overflow-auto items-start">
+    <div
+        v-for="column in columns"
+        :key="column.id"
+        class="column bg-gray-200 p-5 rounded min-w-[250px]"
+    >
+      <header>
+        {{ column.title }}
+      </header>
+
+      <p v-for="task in column.tasks" :key="task.id">
+        {{ task.title }}
+      </p>
+    </div>
+  </div>
+</template>
